02753607d37be055b1750e0dfc739a9579c44350
Mikhail Tyuftyaev <tyuftiaev@mail.ru>
2022-06-22 17:11:43 +0300

    Update styled-left-menu.js

--- src/sub-components/single-content/left-menu/styled-left-menu.js
+++ src/sub-components/single-content/left-menu/styled-left-menu.js
@@ -9,8 +9,12 @@ const StyledLeftMenu = styled.div`
 
 
     .external-link {
+        font-size: 13px;
         color: #333;
         text-decoration: none;
+        margin: 2px 0;
+        padding: 6px 0;
+        display: block;
         :hover {
             color: rgb(255, 111, 61);
         }
