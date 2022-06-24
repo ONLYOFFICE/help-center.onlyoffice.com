77284e714420edcb8c71675130ce082772a501e7
Mikhail Tyuftyaev <tyuftiaev@mail.ru>
2022-06-24 10:31:20 +0300

    Create carousel component

---
+++ components/carousel/sub-components/slide.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Box from "../../box";
+import StyledSlide from "./styled-slide";
+import Link from "../../link";
+
+const Slide = ({ t, currentLanguage, arrayItems, description, ...rest }) => {
+  const { name_video, pathName, imgUrlCard } = arrayItems;
+
+  return (
+    <StyledSlide {...rest} imgUrlCard={imgUrlCard}>
+      <Link href={pathName} className="image-boxshadow-template">
+        <div className="video-image"></div>
+      </Link>
+      <Box
+        className="card-template"
+        flexDirection="column"
+        alignItems="stretch"
+      >
+        <Link
+          className="title-template"
+          title={name_video}
+          href={pathName}
+          label={name_video}
+        />
+      </Box>
+    </StyledSlide>
+  );
+};
+
+
+export default Slide;
