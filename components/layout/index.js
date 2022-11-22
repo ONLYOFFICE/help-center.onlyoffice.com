624f3f5c3662f49f77721678d6742d809ed127d5
Denis <denistyan16@gmail.com>
2022-11-22 13:40:10 +0500

    fix(layout): fix footer content

--- components/layout/index.js
+++ components/layout/index.js
@@ -86,9 +86,12 @@ class Layout extends React.Component {
 
         <Header>{headerContent ? headerContent.props.children : null}</Header>
         <Main>{mainContent ? mainContent.props.children : null}</Main>
-        <Footer className="footer">
-          {footerContent ? footerContent.props.children : null}
-        </Footer>
+        {
+          footerContent &&
+          <Footer className="footer">
+            {footerContent ? footerContent.props.children : null}
+          </Footer>
+        }
       </StyledLayout>
     );
   }
