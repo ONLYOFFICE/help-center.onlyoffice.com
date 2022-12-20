cbb4124cdc591359b6911795591f52553c7f15d5
Denis <denistyan16@gmail.com>
2022-12-20 18:39:18 +0500

    fix(left-menu): fix search icon

--- components/screens/single-content/left-menu/sub-components/search/index.js
+++ components/screens/single-content/left-menu/sub-components/search/index.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import TextInput from "@components/common/text-input";
 import StyledSearch from "./styled-search";
 import SearchIcon from "@public/images/icons/search-icon.react.svg";
+import { ReactSVG } from "react-svg";
 
 const MiniSearch = ({ valueSearch, callback, t, label }) => {
 
@@ -25,7 +26,7 @@ const MiniSearch = ({ valueSearch, callback, t, label }) => {
           labelColor={!valueSearch ? "#808080" : "#CCCCCC"}
         />
         <div className="search_icon">
-            <SearchIcon style={{ cursor: "default" }} />
+            <ReactSVG src={SearchIcon.src} style={{ cursor: "default" }} />
         </div>
     </StyledSearch>
   );
