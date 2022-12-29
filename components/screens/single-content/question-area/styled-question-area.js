c316f80a12a64a0f32a692768fd8f2b8d62b6be4
Denis <denistyan16@gmail.com>
2022-12-29 16:09:15 +0500

    feat: add question-area component

---
+++ components/screens/single-content/question-area/styled-question-area.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import question_forum from "@public/images/icons/question-forum.svg";
+
+const StyledQuestionArea = styled.div`
+  border: 1px solid #EFEFEF;
+  border-radius: 3px;
+  display: grid;
+  align-items: center;
+  grid-template-columns: 49.084% 48.52%;
+  gap: 19px;
+  padding: 23px 40px 33px 79px;
+  background-color: #F5F5F5;
+
+  .question-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .question-title {
+    margin-bottom: 8px;
+    font-size: 24px;
+    line-height: 32px;
+    letter-spacing: -0.02em;
+  }
+
+  .question-text {
+    font-size: 16px;
+    line-height: 26px;
+  }
+
+  .question-link {
+    display: block;
+    font-size: 16px;
+    line-height: 26px;
+  }
+
+  .question-img {
+    width: 100%;
+    max-width: 344px;
+    padding-bottom: 74.21%;
+    background-image: url(${question_forum.src});
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+`;
+
+export default StyledQuestionArea;
