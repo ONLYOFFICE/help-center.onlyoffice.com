d7b43f587f91e68b207a6b5b4f5a372bebd3a2d5
julmaystrovich <Maystrovich.Julia@onlyoffice.com>
2022-12-30 13:21:43 +0300

    add 404

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import Layout from "@components/layout";
+import Error404 from "@components/screens/404-page";
+
+const Error404Page = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      <Layout.PageHead></Layout.PageHead>
+      <Layout.SectionMain>
+        <Error404 t={t} />
+      </Layout.SectionMain>
+    </Layout>
+  );
+};
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, "common")),
+  },
+});
+
+export default Error404Page;
