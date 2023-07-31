58d3f162967cded1ab815927726381ce83750031
julmaystrovich <Maystrovich.Julia@onlyoffice.com>
2023-07-31 18:33:57 +0400

    Update index.js

--- components/screens/main-content/guides-cards/sub-components/guides-links/index.js
+++ components/screens/main-content/guides-cards/sub-components/guides-links/index.js
@@ -5,42 +5,42 @@ import Box from "@components/common/box";
 import InternalLink from "@components/common/internal-link";
 
 const GuidesLinks = ({ mainArticles, linksLength, category, t }) => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const sortedData = sortDataByTopLevel(mainArticles);
-    setData(sortedData);
-  }, []);
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const sortedData = sortDataByTopLevel(mainArticles);
+  //   setData(sortedData);
+  // }, []);
 
-  function sortDataByTopLevel(data) {
-    const sortedData = {};
-    data.forEach(item => {
-      const url = item.attributes.url;
-      const pathParts = url.split('/').filter(part => part !== '');
-      if (pathParts.length >= 2) {
-        const topLevel = pathParts.slice(0, 2).join('/');
-        const topLevelData = data.find(item => {
-          const url = item.attributes.url;
-          const pathParts = url.split('/').filter(part => part !== '');
-          return pathParts.length === 2 && url.includes(topLevel);
-        });
-        if (topLevelData && !sortedData[topLevel]) {
-          sortedData[topLevel] = {
-            name: topLevelData.attributes.title,
-            url: topLevelData.url || `/${topLevel}`,
-            items: [],
-          };
-        }
-        sortedData[topLevel].items?.push(item);
-        if (topLevelData && item.attributes.url === topLevelData.attributes.url) {
-          sortedData[topLevel].items.pop();
-        }
-      }
-    });
-    return Object.values(sortedData);
-  }
+  // function sortDataByTopLevel(data) {
+  //   const sortedData = {};
+  //   data.forEach(item => {
+  //     const url = item.attributes.url;
+  //     const pathParts = url.split('/').filter(part => part !== '');
+  //     if (pathParts.length >= 2) {
+  //       const topLevel = pathParts.slice(0, 2).join('/');
+  //       const topLevelData = data.find(item => {
+  //         const url = item.attributes.url;
+  //         const pathParts = url.split('/').filter(part => part !== '');
+  //         return pathParts.length === 2 && url.includes(topLevel);
+  //       });
+  //       if (topLevelData && !sortedData[topLevel]) {
+  //         sortedData[topLevel] = {
+  //           name: topLevelData.attributes.title,
+  //           url: topLevelData.url || `/${topLevel}`,
+  //           items: [],
+  //         };
+  //       }
+  //       sortedData[topLevel].items?.push(item);
+  //       if (topLevelData && item.attributes.url === topLevelData.attributes.url) {
+  //         sortedData[topLevel].items.pop();
+  //       }
+  //     }
+  //   });
+  //   return Object.values(sortedData);
+  // }
   return (
     <StyledGuidesLinks linksLength={linksLength}>
-      {category === "mobile" && <div className="cell_link cell_heading iOS not_bold">iOS</div>}
+      {/* {category === "mobile" && <div className="cell_link cell_heading iOS not_bold">iOS</div>}
       {!(category === "connectors") && data.map((group, idx) => (
         <div className={`cell_link`} key={idx}>
           <InternalLink label={t(group.name)} href={group.url} />
@@ -62,7 +62,7 @@ const GuidesLinks = ({ mainArticles, linksLength, category, t }) => {
           <InternalLink label={t(item.attributes.title)} href={item.attributes.url} />
         </div>))}
       </Box>
-      }
+      } */}
     </StyledGuidesLinks>
   );
 };
