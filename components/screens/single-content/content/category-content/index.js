2b333a4f9ba68d2d2b88255244a12a8b03e3f165
Juli Vytnova <Maystrovich.Julia@onlyoffice.com>
2023-12-02 19:17:38 +0700

    add docspace

--- components/screens/single-content/content/category-content/index.js
+++ components/screens/single-content/content/category-content/index.js
@@ -1,6 +1,6 @@
 import Heading from "@components/common/heading";
 import Text from "@components/common/text";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactHtmlParser from "react-html-parser";
 import StyledContent from "../styled-content";
 import Breadcrumbs from "@components/common/breadcrumbs";
@@ -16,27 +16,23 @@ const CenterCategoryContent = ({
   children,
   currentLanguage
 }) => {
+  const [artData, setArtData] = useState(null);
   const catData = categories?.find(
     (it) => it.attributes.slug_id === category
   )?.attributes;
    const data = filterAricles(articles, catData.slug_id);
-   //console.log(data);
+   
+   // function to make articles group by first level of url
    const groupArticlesBySecondLevel = () => {
     const groupedArticles = {};
 
     data.forEach((article) => {
       const urlParts = article.attributes.url.split('/');
-
-      // Проверяем, что у нас есть достаточно уровней вложенности
       if (urlParts.length >= 3) {
         const secondLevel = urlParts[2];
-
-        // Создаем группу, если ее еще нет
         if (!groupedArticles[secondLevel]) {
           groupedArticles[secondLevel] = [];
         }
-
-        // Добавляем статью в соответствующую группу
         groupedArticles[secondLevel].push(article);
       }
     });
@@ -46,38 +42,28 @@ const CenterCategoryContent = ({
 
   const groupedArticles = groupArticlesBySecondLevel();
   console.log(groupedArticles);
-  //  const pathnames = data?.attributes.url.split("/").slice(2, 6).filter((x) => x);
-  //  pathnames?.map((name, index) => {
-  //    const routeTo = `/${pathnames.slice(0)}`;
- 
-  //    //console.log(routeTo);
-  //    return routeTo;}
-  //    );
-  //    console.log(pathnames);
-  //    const [filteredArticles, setFilteredArticles] = useState([]);
-  //    useEffect(() => {
-  //      // Фильтруем статьи по второму уровню вложенности в URL
-  //      const filtered = data.filter((article) => {
-  //        const urlParts = article.attributes.url.split('/');
-  //        return urlParts.length >= 3 && urlParts[2] === data.attributes.url;
-  //      });
-  //      console.log(filtered);
-   
-  //      setFilteredArticles(filtered);
-  //    }, [data]);
+
+  useEffect(() => {
+    if (catData.slug_id !== "connectors") {
+      setArtData(groupedArticles);
+    } else {
+      setArtData(data);
+    }
+  }, [catData, articles]);
+  console.log(artData);
   return (
     <StyledContent className="wrapper">
-      {/* <Breadcrumbs t={t} category={catData} isCategory={true} />
+      <Breadcrumbs t={t} category={catData} isCategory={true} />
       <Heading level={3}>{catData?.name}</Heading>
       <Text>{ReactHtmlParser(catData?.description)}</Text>
-      {artData.map((it, index) => {
+      {/* {artData.map((it, index) => {
           return (
             <React.Fragment key={index}>
               {catData.slug_id !== "connectors" ? <CategoryItem data={it} t={t} currentLanguage={currentLanguage} /> : <CategoryConnectorItem data={it} key={index} t={t} currentLanguage={currentLanguage} />}
             </React.Fragment>
            );
-      })}
-      {children} */}
+      })} */}
+      {children}
     </StyledContent>
   );
 };
--- components/screens/single-content/content/sub-components/connector-category-item/index.js
+++ components/screens/single-content/content/sub-components/connector-category-item/index.js
@@ -4,9 +4,6 @@ import Text from "@components/common/text";
 import React, { useState, useEffect } from "react";
 import StyledCategoryItem from "../category-item/styled-category-item";
 import ReactHtmlParser from "react-html-parser";
-import info from "@public/images/icons/info.react.svg";
-import video from "@public/images/icons/video.svg";
-import gs from "@public/images/icons/start.svg";
 import RawHtmlStyle from "@components/utils/rawHtmlStyles";
 import leftMenuGenerating from '@utils/helpers/leftMenuGenerating';
 
@@ -33,32 +30,40 @@ const CategoryItem = ({
   }, [htmlString]);
 
   // for release bottom menu from all headings
-  const itemsBM = leftMenuGenerating(data, false, data.attributes.url, 'h2, h3');
+  const itemsBM = leftMenuGenerating(data, false, data.attributes.url, 'h4, h5');
 
   const gsItem = h4List.find(item => item.href.includes(t('GettingStartedId')));
   const catUrlGS = gsItem ? gsItem.href : null;
   const aboutItem = h4List.find(item => item.href.includes(t('AboutId')));
   const catUrlAbout = aboutItem ? aboutItem.href : null;
   const catUrlVideo = data?.attributes.url + "#watchvideo";
-  const categoryPic = catPic?.find((it) => it.attributes.name === "connector_img.png");
+  const categoryPic = catPic?.find((it) => it.attributes.name === "connector_img.svg");
   return (
     <StyledCategoryItem>
       <Heading level={4}><Link href={data.attributes.url}>{data.attributes.title}</Link><img src={categoryPic?.attributes.url} /></Heading>
       <div className="main_links">
-        {catUrlGS && <div><img src={gs.src} /><Link href={catUrlGS}>{t("GettingStarted")}</Link></div>}
-        {catUrlAbout && <div><img src={info.src} /><Link href={catUrlAbout}>{t("About")}</Link></div>}
-        {catVideo > 0 && <div><img src={video.src} /><Link href={catUrlVideo}>{t("WatchVideo")}</Link></div>}
+        {catUrlGS && <div><img src={'https://static-helpcenter.onlyoffice.com/images/icons/start.react.svg'} /><Link href={catUrlGS}>{t("GettingStarted")}</Link></div>}
+        {catUrlAbout && <div><img src={'https://static-helpcenter.onlyoffice.com/images/icons/info.react.svg'} /><Link href={catUrlAbout}>{t("About")}</Link></div>}
+        {catVideo > 0 && <div><img src={'https://static-helpcenter.onlyoffice.com/images/icons/video.react.svg'} /><Link href={catUrlVideo}>{t("WatchVideo")}</Link></div>}
       </div>
      <Text><RawHtmlStyle>{ReactHtmlParser(data.attributes.description)}</RawHtmlStyle></Text> 
-     {itemsBM.length !== 0 && <>
-     {/* <Heading level={5}>{t("Connecting")}</Heading> */}
-     <ul>
-        {itemsBM.map((link, index) => (
-          <li key={index}>
-            <a href={currentLanguage === "en" ? link.href : `/${link.href}`}>{link.text}</a>
-          </li>
-        ))}
-      </ul></>}
+     {itemsBM.length !== 0 && (
+        <ul>
+          {itemsBM.map((link, index) => (
+            <React.Fragment key={index}>
+              {link.type === "h4" ? (
+                <li>
+                  <a href={currentLanguage === "en" ? link.href : `/${link.href}`}><Heading level={5}>{link.text}</Heading></a>
+                </li>
+              ) : link.type === "h5" && (
+                <li className="sublink">
+                  <a href={currentLanguage === "en" ? link.href : `/${link.href}`}>{link.text}</a>
+                </li>
+              )}
+            </React.Fragment>
+          ))}
+        </ul>
+      )}
     </StyledCategoryItem>
   );
 };
