69ac85deb125ac5a5d762b639c4c7eb885eec3a7
DenisTyan <denistyan16@gmail.com>
2024-07-11 12:45:28 +0500

    refactor: functions page refactor

--- pages/functions/[function]/index.js
+++ pages/functions/[function]/index.js
@@ -1,40 +1,42 @@
-import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import getAllArticles from "@lib/strapi/getFunctions";
-import getAllCategories from "@lib/strapi/getCategories";
+import getCategoriesMenu from "@lib/strapi/getCategoriesMenu";
+import getFunctions from "@lib/strapi/getFunctions";
 
 import Layout from "@components/layout";
 import HeadingContent from "@components/screens/header-content";
 import Footer from "@components/screens/footer-content";
 import SingleContent from "@components/screens/single-page-content";
 import HeadSEO from "@components/screens/head-content";
-import withErrorHandling from "@components/common/hoc/error-handling";
 
-const articlePage = ({ data }) => {
-const { t } = useTranslation();
-const { articles, allCategories, locale } = data;
-const pageTitle = `${t(articles.data[0].attributes.title)} - ONLYOFFICE`;
+const FunctionsPage = ({ locale, categoriesMenu, functions }) => {
+  const { t } = useTranslation();
+  const seoTitle = `${functions.data[0].attributes.title} - ONLYOFFICE`;
 
   return (
     <Layout>
       <Layout.PageHead>
         <HeadSEO
-          title={pageTitle}
-          metaKeywords={pageTitle}
+          title={seoTitle}
+          metaKeywords={seoTitle}
           currentLanguage={locale}
         />
       </Layout.PageHead>
       <Layout.PageHeader>
-        <HeadingContent t={t} template={false} currentLanguage={locale} categories={allCategories.data} />
+        <HeadingContent
+          t={t}
+          template={false}
+          currentLanguage={locale}
+          categories={categoriesMenu.data}
+        />
       </Layout.PageHeader>
       <Layout.SectionMain>
         <SingleContent
           t={t}
           currentLanguage={locale}
-          article={articles.data[0]}
-          tags={articles.data[0]?.attributes.tags?.data}
-          isCategory={false}
+          pageName={functions.data[0].attributes.title}
+          pageDescription={functions.data[0].attributes.content}
+          tags={functions.data[0].attributes.tags}
         />
       </Layout.SectionMain>
       <Layout.PageFooter>
@@ -44,32 +46,24 @@ const pageTitle = `${t(articles.data[0].attributes.title)} - ONLYOFFICE`;
   );
 };
 
-export async function getServerSideProps({ locale, req }) {
-  const [articles, allCategories] = await Promise.all([
-    getAllArticles(locale, req.url.slice(1) || ''),
-    getAllCategories(locale)
-  ]);
+export async function getServerSideProps({ locale, params }) {
+  const categoriesMenu = await getCategoriesMenu(locale);
+  const functions = await getFunctions(locale, `/functions/${params.function}`);
 
-  if (!articles || !allCategories) {
-    res.statusCode = 404;
+  if (functions.data.length === 0) {
     return {
-      props: {
-        ...(await serverSideTranslations(locale, 'common')),
-        data: null,
-      },
+      notFound: true
     };
   }
 
   return {
     props: {
       ...(await serverSideTranslations(locale, "common")),
-      data: {
-        locale,
-        articles,
-        allCategories
-      },
+      locale,
+      categoriesMenu,
+      functions
     },
   };
 }
 
-export default withErrorHandling(articlePage);
+export default FunctionsPage;
