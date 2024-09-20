f6cd870f0b5096b4a73e68d67f8597c4d39ab0df
DenisTyan <denistyan16@gmail.com>
2024-09-20 13:17:10 +0500

    feat(breadcrumbs): add text trim for last title, add styles

--- components/screens/common/breadcrumbs/index.js
+++ components/screens/common/breadcrumbs/index.js
@@ -16,6 +16,28 @@ const Breadcrumbs = forwardRef(({
     level4CategoryName,
     level4CategoryUrl
   }, ref) => {
+  const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) return text;
+  
+    const words = text.split(' ');
+    let start = [], end = [], length = 0;
+  
+    for (let word of words) {
+      if (length + word.length + 1 > Math.floor(maxLength / 2)) break;
+      start.push(word);
+      length += word.length + 1;
+    }
+  
+    length = 0;
+    for (let i = words.length - 1; i >= 0; i--) {
+      if (length + words[i].length + 1 > Math.floor(maxLength / 2)) break;
+      end.unshift(words[i]);
+      length += words[i].length + 1;
+    }
+  
+    return `${start.join(' ')}...${end.join(' ')}`;
+  };
+
   return (
     <StyledBreadcrumbs ref={ref}>
       <li><InternalLink className="breadcrumb-link" href="/" label={t("Home")} /></li>
@@ -34,7 +56,7 @@ const Breadcrumbs = forwardRef(({
       {level4CategoryUrl &&
         <li><InternalLink className="breadcrumb-link" href={level4CategoryUrl} label={level4CategoryName} /></li>
       }
-      <li><span className="breadcrumb-link last">{pageName}</span></li>
+      <li><span className="breadcrumb-link last">{truncateText(pageName, 32)}</span></li>
     </StyledBreadcrumbs>
   );
 });
