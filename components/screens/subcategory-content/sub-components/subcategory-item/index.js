07b341bb8e93e061ab0b2b1a153ef969808244ab
DenisTyan <denistyan16@gmail.com>
2024-09-27 13:15:20 +0500

    feat(subcategory-content): add check isExternalLink for links

--- components/screens/subcategory-content/sub-components/subcategory-item/index.js
+++ components/screens/subcategory-content/sub-components/subcategory-item/index.js
@@ -1,8 +1,17 @@
 import StyledSubCategoryItem from "./styled-subcategory-item";
+import { useState, useEffect } from "react";
 import Heading from "@components/common/heading";
+import ExternalLink from "@components/common/external-link";
 import InternalLink from "@components/common/internal-link";
+import { isExternalLink } from "@utils/helpers/System/isExternal";
 
 const SubCategoryItem = ({ categorySlug, sortItems, id, headingName, headingIcon, links, ...rest }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const checkTitleLength = !links.some(item => {
     const title = item.attributes.title;
     return title && title.replace(/\s/g, "").length > 40;
@@ -24,14 +33,22 @@ const SubCategoryItem = ({ categorySlug, sortItems, id, headingName, headingIcon
             <ul className="subcategory-item-list">
               {links?.slice(0, Math.ceil(links?.length / 2)).sort(sortItems).map((item, index) => (
                 <li key={index}>
-                  <InternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                  {isClient && isExternalLink(item.attributes?.url) ? (
+                    <ExternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                  ) : (
+                    <InternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                  )}
                 </li>
               ))}
             </ul>
             <ul className="subcategory-item-list">
               {links?.slice(Math.ceil(links.length / 2)).sort(sortItems).map((item, index) => (
                 <li key={index}>
-                  <InternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                  {isClient && isExternalLink(item.attributes?.url) ? (
+                    <ExternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                  ) : (
+                    <InternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                  )}
                 </li>
               ))}
             </ul>
@@ -40,7 +57,11 @@ const SubCategoryItem = ({ categorySlug, sortItems, id, headingName, headingIcon
           <ul className="subcategory-item-list">
             {links.sort(sortItems).map((item, index) => (
               <li key={index}>
-                <InternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                {isClient && isExternalLink(item.attributes?.url) ? (
+                  <ExternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                ) : (
+                  <InternalLink href={item.attributes.url} label={item.attributes.name || item.attributes.title} />
+                )}
               </li>
             ))}
           </ul>
